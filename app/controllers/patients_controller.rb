454235734fb0e02f4e75a53ead5e38e54b24e2cc
AnithaPal <anitha.aravindvel@gmail.com>
2016-03-05 12:00:01 -0500

    patient profile craetion form added

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -58,7 +58,7 @@ class PatientsController < ApplicationController
   private
 
   def patient_params
-    params.require(:patient).permit(:name, :email, :phone_no, :home_address, :facility_address, :pickup_time, :days_of_week)
+    params.require(:patient).permit(:name, :email, :phone_no, :home_address, :facility_address, :pickup_time, days_of_week:[])
   end
 
 end
