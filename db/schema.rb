5341552a51edf26d6813b550a108b15497f317af
John Kirkley <john@johnkirkley.com>
2016-03-05 12:42:45 -0500

    removed import clinics

--- db/schema.rb
+++ db/schema.rb
@@ -22,26 +22,10 @@ ActiveRecord::Schema.define(version: 20160305152116) do
     t.integer  "beds"
     t.string   "telephone"
     t.string   "type"
+    t.float    "latitude"
+    t.float    "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "patients", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "patients", ["email"], name: "index_patients_on_email", unique: true
-  add_index "patients", ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true
 
 end
