de34ad9906778ab7575e6577e08d500d87ee07d1
John Kirkley <john@johnkirkley.com>
2016-03-05 15:21:26 -0500

    imported dialysis clinics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305152116) do
+ActiveRecord::Schema.define(version: 20160305194221) do
 
   create_table "facilities", force: :cascade do |t|
     t.string   "name"
@@ -21,11 +21,11 @@ ActiveRecord::Schema.define(version: 20160305152116) do
     t.string   "zip_code"
     t.integer  "beds"
     t.string   "telephone"
-    t.string   "type"
+    t.string   "facility_type"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "patients", force: :cascade do |t|
